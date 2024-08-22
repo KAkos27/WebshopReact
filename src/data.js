f1c3d2776da405ac7f56ee38e959ebc85a2d2713
KAkos27 <akoskosztolanyi@gmail.com>
2024-08-22 11:18:44 +0200

    dummy data létrehozva

---
+++ src/data.js
@@ -0,0 +1,75 @@
+import t0Src from "./assets/t0.jpg";
+import t1Src from "./assets/t1.jpg";
+import t2Src from "./assets/t2.jpg";
+import t3Src from "./assets/t3.jpg";
+import t4Src from "./assets/t4.jpg";
+import t5Src from "./assets/t5.jpg";
+import t6Src from "./assets/t6.jpg";
+import t7Src from "./assets/t7.jpg";
+
+export const TOYS = [
+  {
+    id: "t0",
+    image: { src: t0Src, alt: "TIAKI Flip-Flop kutyajáték" },
+    name: "TIAKI Flip-Flop kutyajáték",
+    info: "Színes flip-flop papucs formájú nyári játék kutyáknak, habszivacs töltettel, úszik a víz felszínén, ideális akciódús vízi játékokhoz, zizegő fóliával, poliészterből",
+    price: 1550,
+    categories: ["playtoy"],
+  },
+  {
+    id: "t1",
+    image: { src: t1Src, alt: "Nomad Tales Spirit úszó dummy" },
+    name: "Nomad Tales Spirit úszó dummy",
+    info: "Izgalmas kutyajáték, egészségre ártalmatlan anyagokból, úszik a vízben, hurokkal, kiválóan dobálható, apportírozható és rángcigáltható, szépen sípol, poliészterből és pamutból",
+    price: 2890,
+    categories: ["playtoy"],
+  },
+  {
+    id: "t2",
+    image: { src: t2Src, alt: "Nomad Tales Bloom snack labda" },
+    name: "Nomad Tales Bloom snack labda",
+    info: "Masszív, pattogó labda kutyáknak hőre lágyuló elasztomerből. sok réssel és nagy lyukkal, megtölthető jutalomfalatokkal, száraztáppal  vagy pasztákkal, az extra hosszú játékélményért, TPE-ből",
+    price: 2450,
+    categories: ["playtoy", "feedingtoy"],
+  },
+  {
+    id: "t3",
+    image: { src: t3Src, alt: "TIAKI bacon ízű rágócsont" },
+    name: "TIAKI bacon ízű rágócsont",
+    info: "Ízesített rágójáték kutyáknak finom bacon ízben. Rágásra ösztönzi kedvencét, bütykös felülettel az íny masszírozásához. Erősíti a fogakat és az ínyt- Fogápoló hatású, nylonból és TPU-ból.",
+    price: 890,
+    categories: ["playtoy", "feedingtoy"],
+  },
+  {
+    id: "t4",
+    image: { src: t4Src, alt: "TPR-csont kötéllel" },
+    name: "TPR-csont kötéllel",
+    info: "Kötél kölyköknek és kisebb kutyáknak, dobálásra és apportírozásra. A csomózott kötél közepén csonttal, noppjai rágásra ösztönöznek és stimulálják a fogínyt. Poliészterből és termoplasztikus gumiból.",
+    price: 1790,
+    categories: ["playtoy"],
+  },
+  {
+    id: "t5",
+    image: { src: t5Src, alt: "Nomad Tales Bloom labdahajító" },
+    name: "Nomad Tales Bloom labdahajító",
+    info: "Labdahajító kutyáknak, megkönnyíti a labda lehajolás nélkül történő felvételét, lehetővé teszi a messzebbre dobálást kevesebb erőfeszítéssel, Ø 6 - 6,5 cm-es labdákhoz, PP és TPR (hőre lágyuló gumi)",
+    price: 1550,
+    categories: ["playtoy"],
+  },
+  {
+    id: "t6",
+    image: { src: t6Src, alt: "Habzsolás elleni acél etetőtál" },
+    name: "Habzsolás elleni acél etetőtál",
+    info: "Rozsdamentes acél etetőtál közepén kidomborodással a gyors evés megakadályozására; akár ötödjére csökkenhet a mohó evés miatti félrenyelés, alján prémium gumigyűrű a stabilitásért; gépben elmosható.",
+    price: 4550,
+    categories: ["feedingtoy"],
+  },
+  {
+    id: "t7",
+    image: { src: t7Src, alt: "Modern Living Brasilia tányér" },
+    name: "Modern Living Brasilia tányér",
+    info: "Színes nyalogató szőnyeg kutyáknak, macskáknak, tökéletes pasztákhoz, segíti a lassú evést, csökkenti a stresszt, szeparációs szorongást és unalmat, fogbarát, élelmiszer-ipari minőségű szilikonból.",
+    price: 1670,
+    categories: ["feedingtoy"],
+  },
+];
